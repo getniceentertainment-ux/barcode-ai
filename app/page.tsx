04895d9614182ba299df31ad0793d1d4a8f6b3be
getniceentertainment-ux <getnice.entertainment@gmail.com>
2026-03-19 17:12:35 -0400

    10

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function MatrixController() {
     hasAccess, activeRoom, setActiveRoom, userSession, clearMatrix, 
     audioData, isProjectFinalized, playbackMode, setPlaybackMode, 
     radioTrack, setRadioTrack, addToast,
-    vocalStems, generatedLyrics, blueprint
+    vocalStems, generatedLyrics, blueprint, isUpgrading
   } = useMatrixStore();
 
   const [isBoosting, setIsBoosting] = useState(false);
