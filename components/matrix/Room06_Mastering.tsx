2dcac80afb00a651822cb2cd81dcdeffcb193cb2
getniceentertainment-ux <getnice.entertainment@gmail.com>
2026-03-23 23:47:07 -0400

    6

--- components/matrix/Room06_Mastering.tsx
+++ components/matrix/Room06_Mastering.tsx
@@ -139,7 +139,7 @@ export default function Room06_Mastering() {
       const fileName = `${userSession?.id || 'ANON'}/${Date.now()}_MASTER.wav`;
       
       const { data, error } = await supabase.storage
-        .from('vault') // <--- Make sure you have a bucket named 'vault' set to Public!
+        .from('submissions') // <--- Make sure you have a bucket named 'vault' set to Public!
         .upload(fileName, wavBlob, {
           contentType: 'audio/wav',
           upsert: true,
@@ -149,7 +149,7 @@ export default function Room06_Mastering() {
 
       // Retrieve the permanent public URL
       const { data: { publicUrl } } = supabase.storage
-        .from('vault')
+        .from('submissions')
         .getPublicUrl(fileName);
 
       setMasterUrl(publicUrl);
